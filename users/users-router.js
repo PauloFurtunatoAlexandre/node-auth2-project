855c3574c919ed1cbab99906623155d6b4d3f500
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-10-04 22:49:44 -0500

    added role validation for Delete, Post and Put methods

--- users/users-router.js
+++ users/users-router.js
@@ -2,13 +2,26 @@ const router = require("express").Router();
 
 const Users = require("./users-model.js");
 const restricted = require("../auth/restricted-middleware.js");
+const checkRole = require("../auth/check-role.js");
 
 router.get("/", restricted, (req, res) => {
-  Users.find()
-    .then(users => {
-      res.status(200).json(users);
-    })
-    .catch(err => res.send(err));
+    Users.find()
+        .then((users) => {
+            res.status(200).json(users);
+        })
+        .catch((err) => res.send(err));
+});
+
+router.post("/:id", restricted, checkRole(1), (req, res) => {
+    res.status(501).json({message: "not implemented."});
+});
+
+router.put("/:id", restricted, checkRole(1), (req, res) => {
+    res.status(501).json({message: "not implemented."});
+});
+
+router.delete("/:id", restricted, checkRole(1), (req, res) => {
+    res.status(501).json({message: "not implemented."});
 });
 
 module.exports = router;
